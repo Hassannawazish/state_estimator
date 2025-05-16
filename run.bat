a02d108451e788decfb9bc86a78c2ca1cb7868d4
Rana Tauseef Ahmed <57061432+ahmedouseef@users.noreply.github.com>
2025-05-16 22:58:18 +0200

    resuts&bat
    resuts&bat

--- run.bat
+++ run.bat
@@ -1,1 +1,9 @@
-python localization.py --plot pf
+@echo off
+
+REM open a new console window for each script:
+start "" python evaluate_ekf.py
+start "" python evaluate_pf.py
+start "" python localization.py --plot pf
+
+REM optional: exit this “launcher” window
+exit
